632ab1627f534875ac626ae129150606d5b9c103
Michael Dodson <thinkwellpub@gmail.com>
2022-01-17 11:28:59 -0600

    Complex App React Project
    Complex App React Project. Updated the login and logout functionality of the app.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import HeaderLogOut from "./HeaderLogOut";
+import HeaderLogIn from "./HeaderLogIn";
 
 function Header() {
+  const [loggedIn, setLoggedIn] = useState();
   return (
     <header className="header-bar bg-primary mb-3">
       <div className="container d-flex flex-column flex-md-row align-items-center p-3">
@@ -11,7 +13,7 @@ function Header() {
             ComplexApp
           </Link>
         </h4>
-        <HeaderLogOut />
+        {loggedIn ? <HeaderLogIn setLoggedIn={setLoggedIn} /> : <HeaderLogOut setLoggedIn={setLoggedIn} />}
       </div>
     </header>
   );
