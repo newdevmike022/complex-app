f5b6dc9a686378d84f44b86c62c55febaeea13bd
Michael Dodson <thinkwellpub@gmail.com>
2022-01-17 18:54:36 -0600

    Complex App React Project
    Complex App React Project. Creating the conditional home page content.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import HeaderLogOut from "./HeaderLogOut";
 import HeaderLogIn from "./HeaderLogIn";
 
-function Header() {
-  const [loggedIn, setLoggedIn] = useState(Boolean(localStorage.getItem("complexappToken")));
+function Header(props) {
   return (
     <header className="header-bar bg-primary mb-3">
       <div className="container d-flex flex-column flex-md-row align-items-center p-3">
@@ -13,7 +12,7 @@ function Header() {
             ComplexApp
           </Link>
         </h4>
-        {loggedIn ? <HeaderLogIn setLoggedIn={setLoggedIn} /> : <HeaderLogOut setLoggedIn={setLoggedIn} />}
+        {props.loggedIn ? <HeaderLogIn setLoggedIn={props.setLoggedIn} /> : <HeaderLogOut setLoggedIn={props.setLoggedIn} />}
       </div>
     </header>
   );
