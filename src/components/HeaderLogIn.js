63ed5d8633176f0ec185359018b900ed896d0b16
Michael Dodson <thinkwellpub@gmail.com>
2022-01-20 11:25:58 -0600

    Complex App React Project
    Complex App React Project. Updated app components with useEffect.

--- src/components/HeaderLogIn.js
+++ src/components/HeaderLogIn.js
@@ -1,14 +1,14 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import DispatchContext from "../DispatchContext";
+import StateContext from "../StateContext";
 
 function HeaderLogIn(props) {
   const appDispatch = useContext(DispatchContext);
+  const appState = useContext(StateContext);
+
   const handleLogout = () => {
     appDispatch({ type: "logout" });
-    localStorage.removeItem("complexappToken");
-    localStorage.removeItem("complexappUsername");
-    localStorage.removeItem("complexappAvatar");
   };
 
   return (
@@ -21,7 +21,7 @@ function HeaderLogIn(props) {
         <span className="chat-count-badge text-white"> </span>
       </span>
       <Link to="#" className="mr-2">
-        <img className="small-header-avatar" src={localStorage.getItem("complexappAvatar")} alt="avatar" />
+        <img className="small-header-avatar" src={appState.user.avatar} alt="avatar" />
       </Link>
       <Link className="btn btn-sm btn-success mr-2" to="/create-post">
         Create Post
