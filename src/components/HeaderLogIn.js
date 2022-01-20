0bcf27fda92e2c94c6f8443e0363efaf4e74b64c
Michael Dodson <thinkwellpub@gmail.com>
2022-01-20 12:44:33 -0600

    Complex App React Project
    Complex App React Project. Creating the Profile Screen.

--- src/components/HeaderLogIn.js
+++ src/components/HeaderLogIn.js
@@ -20,7 +20,7 @@ function HeaderLogIn(props) {
         <i className="fas fa-comment"></i>
         <span className="chat-count-badge text-white"> </span>
       </span>
-      <Link to="#" className="mr-2">
+      <Link to={`/profile/${appState.user.username}`} className="mr-2">
         <img className="small-header-avatar" src={appState.user.avatar} alt="avatar" />
       </Link>
       <Link className="btn btn-sm btn-success mr-2" to="/create-post">
